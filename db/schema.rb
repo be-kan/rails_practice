c05da478a0109fff0b4efb517b84a95b556662ab
神戸隆太 <kambe3141@gmail.com>
2018-03-02 15:35:47 +0900

    sqlite to mysql!!

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 
 ActiveRecord::Schema.define(version: 20180222141859) do
 
-  create_table "users", force: :cascade do |t|
+  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
